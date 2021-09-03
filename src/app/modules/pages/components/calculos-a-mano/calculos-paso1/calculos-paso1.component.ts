fbefc6d33e5fe956326c839c8f2d894484ad5171
Daniel Martinez <daniel.martinez.am@gmail.com>
2021-09-03 12:52:48 -0300

    datos de tabla hardcodeada mas reales

--- src/app/modules/pages/components/calculos-a-mano/calculos-paso1/calculos-paso1.component.ts
+++ src/app/modules/pages/components/calculos-a-mano/calculos-paso1/calculos-paso1.component.ts
@@ -8,7 +8,7 @@ interface Edad {
 }
 
 interface EdadCantMediana {
-  edad: number;
+  edad: string;
   cantidad: number;
   mediana: number;
 }
@@ -34,7 +34,24 @@ export class CalculosPaso1Component implements AfterViewInit {
     {value: 'meses-11', viewValue: '11 Meses'},
     {value: 'anios-1', viewValue: '1 Año'},
     {value: 'anios-2', viewValue: '2 Años'},
-    {value: 'anios-3', viewValue: '3 Años'}
+    {value: 'anios-3', viewValue: '3 Años'},
+    {value: 'anios-4', viewValue: '4 Años'},
+    {value: 'anios-5', viewValue: '5 Años'},
+    {value: 'anios-6', viewValue: '6 Años'},
+    {value: 'anios-7', viewValue: '7 Años'},
+    {value: 'anios-8', viewValue: '8 Años'},
+    {value: 'anios-9', viewValue: '9 Años'},
+    {value: 'anios-10', viewValue: '10 Años'},
+    {value: 'anios-11', viewValue: '11 Años'},
+    {value: 'anios-12', viewValue: '12 Años'},
+    {value: 'anios-13', viewValue: '13 Años'},
+    {value: 'anios-14', viewValue: '14 Años'},
+    {value: 'anios-15', viewValue: '15 Años'},
+    {value: 'anios-16', viewValue: '16 Años'},
+    {value: 'anios-17', viewValue: '17 Años'},
+    {value: 'anios-18-29', viewValue: '18-29 Años'},
+    {value: 'anios-30-59', viewValue: '30-59 Años'},
+    {value: 'anios-60+', viewValue: '60+ Años'},
   ];
 
   displayedColumns: string[] = ['edad', 'cantidad', 'mediana'];
@@ -45,28 +62,33 @@ export class CalculosPaso1Component implements AfterViewInit {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
 
 const FEMENINO_DATA: EdadCantMediana[] = [
-  {edad: 1, cantidad: 151, mediana: 1.09},
-  {edad: 2, cantidad: 441, mediana: 4.06},
-  {edad: 3, cantidad: 411, mediana: 6.91},
-  {edad: 4, cantidad: 846, mediana: 9.22},
-  {edad: 5, cantidad: 456, mediana: 10.11},
-  {edad: 6, cantidad: 894, mediana: 12.07},
-  {edad: 7, cantidad: 120, mediana: 14.67},
-  {edad: 8, cantidad: 784, mediana: 15.94},
-  {edad: 9, cantidad: 9561, mediana: 18.84},
-  {edad: 10, cantidad: 7984, mediana: 20.97},
-  {edad: 11, cantidad: 845, mediana: 22.97},
-  {edad: 12, cantidad: 1511, mediana: 24.05},
-  {edad: 13, cantidad: 946, mediana: 26.15},
-  {edad: 14, cantidad: 496, mediana: 28.05},
-  {edad: 15, cantidad: 794, mediana: 30.38},
-  {edad: 16, cantidad: 48, mediana: 32.05},
-  {edad: 17, cantidad: 749, mediana: 35.43},
-  {edad: 18, cantidad: 554, mediana: 39.98},
-  {edad: 19, cantidad: 461, mediana: 39.83},
-  {edad: 20, cantidad: 1288, mediana: 40.78}
+  {edad: '1 Año', cantidad: 151, mediana: 9.5},
+  {edad: '2 Años', cantidad: 441, mediana: 12.0},
+  {edad: '3 Años', cantidad: 411, mediana: 14.2},
+  {edad: '4 Años', cantidad: 846, mediana: 15.4},
+  {edad: '5 Años', cantidad: 456, mediana: 17.9},
+  {edad: '6 Años', cantidad: 894, mediana: 19.9},
+  {edad: '7 Años', cantidad: 120, mediana: 22.4},
+  {edad: '8 Años', cantidad: 784, mediana: 25.8},
+  {edad: '9 Años', cantidad: 9561, mediana: 28.1},
+  {edad: '10 Años', cantidad: 7984, mediana: 31.9},
+  {edad: '11 Años', cantidad: 845, mediana: 36.9},
+  {edad: '12 Años', cantidad: 1511, mediana: 41.5},
+  {edad: '13 Años', cantidad: 946, mediana: 45.8},
+  {edad: '14 Años', cantidad: 496, mediana: 47.6},
+  {edad: '15 Años', cantidad: 794, mediana: 52.1},
+  {edad: '16 Años', cantidad: 48, mediana: 53.5},
+  {edad: '17 Años', cantidad: 749, mediana: 54.4},
+  {edad: '18-29 Años', cantidad: 554, mediana: 60.0},
+  {edad: '30-59 Años', cantidad: 461, mediana: 60.0},
+  {edad: '60+ Años', cantidad: 1288, mediana: 60.0}
 ];
 
--- src/app/modules/pages/pages-routing.module.ts
+++ src/app/modules/pages/pages-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [{
     pathMatch: 'full'
   }]
 }];
+
 @NgModule({
   imports: [
     RouterModule.forChild(routes)
