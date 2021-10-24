a302a3c71d68016a1fdbd29dab56bf796d8a1fce
Belén Echechury <belenechechury@gmail.com>
2021-10-24 17:40:42 -0300

    auditoria sin testeo de integración, despliega tabla y total de resultados

---
+++ src/app/models/auditory-search.model.spec.ts
@@ -0,0 +1,7 @@
+import { AuditorySearch } from './auditory-search.model';
+
+describe('AuditorySearch', () => {
+  it('should create an instance', () => {
+    expect(new AuditorySearch()).toBeTruthy();
+  });
+});
