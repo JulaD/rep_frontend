b4f70465f04a7768a380f3df14aa99bb707dea25
Ramiro <rbentancor@globaluy.com>
2021-10-24 19:01:30 -0300

    JWT in services

--- src/app/services/values.service.ts
+++ src/app/services/values.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import DefaultExtraDataDTO from '../interfaces/DefaultExtraDataDTO';
@@ -14,11 +14,23 @@ export class ValuesService {
     private http: HttpClient,
   ) { }
 
+  private options = () => {
+    let token: string = '';
+    if (localStorage.getItem('token')) {
+      token = String(localStorage.getItem('token'));
+    }
+    return {
+      headers: new HttpHeaders({
+        Authorization: token,
+      }),
+    };
+  };
+
   getParameters() {
-    return this.http.get<any>('http://localhost:8000/parameters');
+    return this.http.get<any>('http://localhost:8000/parameters', this.options());
   }
 
   modifyParameter(parameters: DefaultWeightDTO[] | DefaultExtraDataDTO[] | EquationConstantDTO[]) {
-    return this.http.put<any>('http://localhost:8000/parameters/parameterUpdate', { parameters });
+    return this.http.put<any>('http://localhost:8000/parameters/parameterUpdate', { parameters }, this.options());
   }
 }
