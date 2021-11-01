cd1a5c4a074c618dd68327f6595f5aec4430a69b
Agustin <agusr1999@gmail.com>
2021-11-01 17:11:15 -0300

    Download progress working in stepper, also looks kinda nice

--- src/app/modules/pages/components/calculos-a-mano/stepper/stepper.component.ts
+++ src/app/modules/pages/components/calculos-a-mano/stepper/stepper.component.ts
@@ -91,7 +91,7 @@ export class StepperComponent implements OnInit, OnDestroy {
     private errorSnackBar: MatSnackBar,
   ) {}
 
-  onSubmit(): void {
+  prepareData() {
     const extraData: ExtraData = {
       minorPAL: undefined,
       adultPAL: undefined,
@@ -118,6 +118,12 @@ export class StepperComponent implements OnInit, OnDestroy {
       }
     }
 
+    return { step1Data, extraData };
+  }
+
+  onSubmit(): void {
+    const { step1Data, extraData } = this.prepareData();
+
     this.rest.addCalculation(step1Data, extraData)
       .subscribe((result) => {
         this.resultsService
@@ -127,6 +133,24 @@ export class StepperComponent implements OnInit, OnDestroy {
       }, (err) => {
         console.log(err);
       });
+  }
+
+  saveProgress() {
+    const progress = this.prepareData();
+
+    const date = new Date();
+    const fileName : string = `ProgresoCalculoREP_${
+      date.getDate()}_${
+      date.getMonth() + 1}_${
+      date.getFullYear()}.pdf`;
+
+    const csv: string = `data:text/json;charset=utf-8,${JSON.stringify(progress)}`;
+    const data = encodeURI(csv);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', fileName);
+    link.click();
   }
 
   isStepperValid(): boolean {
