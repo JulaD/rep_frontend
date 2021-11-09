d5405c3a8953bf501bb3e500d46a88d6367018a1
Belén Echechury <belenechechury@gmail.com>
2021-11-09 00:28:34 -0300

    enum con filtros para logs

---
+++ src/app/enums/log-filter.ts
@@ -0,0 +1,6 @@
+export enum LogFilter {
+    ModificacionConstantes = "Modificación de constantes de ecuación",
+    ModificacionDatosDefecto = "Cambio de datos por defecto",
+    PermisosAdministrador = "Manejo de permisos de administrador",
+    AccesoAplicacion = "Manejo de acceso a la aplicación"
+}
