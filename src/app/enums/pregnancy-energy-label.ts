ec059715a93c3c9e53bcfe073a78ea6418b35707
Belén Echechury <belenechechury@gmail.com>
2021-11-12 08:19:53 -0300

    enums con labels para cambio de valores

---
+++ src/app/enums/pregnancy-energy-label.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-shadow
+export enum PregnancyEnergyLabel {
+  lactationExtraEnergy30to59 = '30 a 59 años - Energía lactancia',
+  pregnancyExtraEnergy30to59 = '30 a 59 años - Energía embarazo',
+  lactationExtraEnergy18to29 = '18 a 29 años - Energía lactancia',
+  pregnancyExtraEnergy18to29 = '18 a 29 años - Energía embarazo'
+}
